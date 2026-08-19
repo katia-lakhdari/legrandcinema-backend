711eeaa3932ac8b5839519e0ea6f03a444ebb24c
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-08-19 23:50:40 +0200

    feat: ajout de l'entité Film

---
+++ src/main/java/com/legrandcinema/entity/Film.java
@@ -0,0 +1,31 @@
+package com.legrandcinema.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "films")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titre;
+
+    @Column(nullable = false)
+    private Integer duree; // en minutes
+
+    private String affiche; // URL de l'image de l'affiche
+
+    @Column(length = 1000)
+    private String description;
+}
