3479809423c789bc00468e0be1fb7ca0e9f2c173
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-08-19 23:46:18 +0200

    feat: ajout de l'entité Utilisateur

---
+++ src/main/java/com/legrandcinema/entity/Utilisateur.java
@@ -0,0 +1,38 @@
+package com.legrandcinema.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "utilisateurs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String motDePasse;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public enum Role {
+        CLIENT,
+        ADMIN
+    }
+}
