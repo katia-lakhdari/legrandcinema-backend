7f3c5be463bcfde9d9fa0010dcc2f9ec81b6b898
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-08-20 13:53:06 +0200

    feat: ajout du repository Film

--- src/main/java/com/legrandcinema/repository/FilmRepository.java
+++ src/main/java/com/legrandcinema/repository/FilmRepository.java
@@ -1,4 +1,7 @@
 package com.legrandcinema.repository;
 
-public interface FilmRepository {
+import com.legrandcinema.entity.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmRepository extends JpaRepository<Film, Long> {
 }
