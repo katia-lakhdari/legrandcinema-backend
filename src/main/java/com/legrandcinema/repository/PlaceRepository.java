9fab607ff9fc52803fc26d4baa1717855a1b67b7
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-08-20 13:54:11 +0200

    feat: ajout du repository Place

--- src/main/java/com/legrandcinema/repository/PlaceRepository.java
+++ src/main/java/com/legrandcinema/repository/PlaceRepository.java
@@ -1,4 +1,7 @@
 package com.legrandcinema.repository;
 
-public interface PlaceRepository {
+import com.legrandcinema.entity.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
 }
