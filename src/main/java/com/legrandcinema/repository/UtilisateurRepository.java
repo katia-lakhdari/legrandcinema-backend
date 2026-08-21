dd0bcfef132a076b615d5f698dc6caacf5af4f62
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-08-21 12:41:24 +0200

    feat: ajout de la methode findByEmail dans UtilisateurRepository

--- src/main/java/com/legrandcinema/repository/UtilisateurRepository.java
+++ src/main/java/com/legrandcinema/repository/UtilisateurRepository.java
@@ -2,6 +2,8 @@ package com.legrandcinema.repository;
 
 import com.legrandcinema.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    Optional<Utilisateur> findByEmail(String email);
 }
