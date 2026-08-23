538b5ad19a531352246db7a5b536cd31fefe29ca
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-08-23 20:57:28 +0200

    feat: ajout de PlaceController avec les routes de verrouillage

---
+++ src/main/java/com/legrandcinema/controller/PlaceController.java
@@ -0,0 +1,43 @@
+package com.legrandcinema.controller;
+
+import com.legrandcinema.entity.Place;
+import com.legrandcinema.dto.response.PlaceResponse;
+import com.legrandcinema.service.PlaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/places")
+public class PlaceController {
+
+    @Autowired
+    private PlaceService placeService;
+
+    @GetMapping("/seance/{seanceId}")
+    public List<PlaceResponse> listerPlacesParSeance(@PathVariable Long seanceId) {
+        List<Place> places = placeService.listerPlacesParSeance(seanceId);
+        List<PlaceResponse> reponses = new ArrayList<>();
+        for (Place place : places) {
+            reponses.add(new PlaceResponse(place.getId(), place.getNumero(),
+                    place.getStatut().name(), place.getFinVerrouillage()));
+        }
+        return reponses;
+    }
+
+    @PostMapping("/{id}/verrouiller")
+    public PlaceResponse verrouillerPlace(@PathVariable Long id) {
+        Place place = placeService.verrouillerPlace(id);
+        return new PlaceResponse(place.getId(), place.getNumero(),
+                place.getStatut().name(), place.getFinVerrouillage());
+    }
+
+    @PostMapping("/{id}/liberer")
+    public PlaceResponse libererPlace(@PathVariable Long id) {
+        Place place = placeService.libererPlace(id);
+        return new PlaceResponse(place.getId(), place.getNumero(),
+                place.getStatut().name(), place.getFinVerrouillage());
+    }
+}
