355e2aabd66d6cd1a67372533e872de1b87043b4
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-09-21 18:10:56 +0200

    feat: ajout de la route de consultation des billets côté client

--- src/main/java/com/legrandcinema/repository/BilletRepository.java
+++ src/main/java/com/legrandcinema/repository/BilletRepository.java
@@ -3,9 +3,11 @@ package com.legrandcinema.repository;
 import com.legrandcinema.entity.Billet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BilletRepository extends JpaRepository<Billet, Long> {
     boolean existsByReservationId(Long reservationId);
     Optional<Billet> findByQrCode(String qrCode);
+    List<Billet> findByReservationUtilisateurId(Long utilisateurId);
 }
