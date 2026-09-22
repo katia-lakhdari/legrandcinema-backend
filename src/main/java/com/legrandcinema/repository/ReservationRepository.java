449a0b28eccef13f51831059f8255df8407bfc83
Katia Lakhdari <katia.lakhdari@gmail.com>
2026-09-22 19:58:08 +0200

    feat: ajout de la recherche des réservations en cours

--- src/main/java/com/legrandcinema/repository/ReservationRepository.java
+++ src/main/java/com/legrandcinema/repository/ReservationRepository.java
@@ -3,5 +3,10 @@ package com.legrandcinema.repository;
 import com.legrandcinema.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findBySeance_DateHeureAfterAndStatutNot(LocalDateTime maintenant, Reservation.StatutReservation statut);
 }
